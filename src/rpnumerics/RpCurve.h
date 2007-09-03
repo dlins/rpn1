414567a4765eca44e6c47cdf2311d4713e291e9a
mvera <mvera@9db63477-e02e-0410-9af5-e195358994a9>
2007-09-03 20:11:41 +0000

    added RpCurve
    git-svn-id: svn://svn.impa.br/rpn/trunk@88 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/rpnumerics/RpCurve.h
@@ -0,0 +1,38 @@
+/**A
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) RpCurve.h
+ **/
+
+
+#ifndef _RpCurve_
+#define	_RpCurve_
+
+#include "RealVector.h"
+
+//! Definition of class RpCurve.
+/*!
+	The RpCurve class will hold the operations to be
+realized upon a curve.
+
+ TODO:
+ NOTE :
+
+ @ingroup rpnumerics
+ */
+
+class RpCurve 
+    
+    
+    public :
+
+        //! concatenation operator
+        const  RpCurve cat(const RpCurve &);
+        
+};
+
+
+#endif	/* _RpCurve_ */
+
