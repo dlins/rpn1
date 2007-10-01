b3caf25a000e7e68f74a32a0f98b45839231321b
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2007-10-01 20:32:41 +0000

    ""
    git-svn-id: svn://svn.impa.br/rpn/trunk@152 9db63477-e02e-0410-9af5-e195358994a9

--- src/rpnumerics/HugoniotCurveCalcND.java
+++ src/rpnumerics/HugoniotCurveCalcND.java
@@ -6,47 +6,55 @@
 
 package rpnumerics;
 
-import rpnumerics.PhasePoint;
 import wave.util.RealVector;
 import wave.util.RealMatrix2;
 import wave.util.VectorFunction;
 
 public class HugoniotCurveCalcND
-    implements HugoniotCurveCalc {
-  //
-  // Constants
-  //
-  static public final double UMINUS_SHIFT = .01;
-  //
-  // Members
-  //
-
-  private VectorFunction f_;
-
-//  private HugoniotMethod hugoniotMethod_;
-
-  private RealVector Uminus_;
-  private RealVector Fminus_;
-  private RealMatrix2 DFminus_;
-
-  public void uMinusChangeNotify(PhasePoint uMinus) {
-
-    setUMinus(uMinus);
-
-  }
-
-  public void setUMinus(PhasePoint pPoint) {
-    Uminus_ = pPoint.getCoords();
-    Fminus_ = rpnumerics.RPNUMERICS.fluxFunction().F(Uminus_);
-    DFminus_ = rpnumerics.RPNUMERICS.fluxFunction().DF(Uminus_);
-  }
-
-  public PhasePoint getUMinus() { return new PhasePoint(Uminus_); }
-  public RealVector getFMinus() { return Fminus_; }
-  public RealMatrix2 getDFMinus() { return DFminus_; }
-  public double[] getPrimitiveUMinus(){return Uminus_.toDouble();}
-
-  public native  RpSolution calc();
-
-  public native  RpSolution recalc() ;
+        implements HugoniotCurveCalc {
+    //
+    // Constants
+    //
+    static public final double UMINUS_SHIFT = .01;
+    //
+    // Members
+    //
+    
+    private VectorFunction f_;
+    
+    private RealVector Uminus_;
+    private RealVector Fminus_;
+    private RealMatrix2 DFminus_;
+    
+    public void uMinusChangeNotify(PhasePoint uMinus) {
+        
+        Uminus_=uMinus;
+        setUMinus(uMinus);
+    }
+    
+    
+    
+    public PhasePoint getUMinus() { return new PhasePoint(Uminus_); }
+    public RealVector getFMinus() { return Fminus_; }
+    public RealMatrix2 getDFMinus() { return DFminus_; }
+    public double[] getPrimitiveUMinus(){return Uminus_.toDouble();}
+    
+    
+    public native void setUMinus(PhasePoint uMinus);
+    
+    public  RpSolution calc(){
+        
+        return calc(Uminus_);
+        
+    }
+    
+    public   RpSolution recalc() {
+        
+        return calc();
+        
+    }
+    
+    private native RpSolution calc(RealVector uMinus);
+    
+    
 }
