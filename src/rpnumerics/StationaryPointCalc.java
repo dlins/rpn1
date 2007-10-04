6dfc41e8feaf6431509088c25f6c9b732779bc97
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2007-10-04 18:10:03 +0000

    ""
    git-svn-id: svn://svn.impa.br/rpn/trunk@182 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/rpnumerics/StationaryPointCalc.java
@@ -0,0 +1,62 @@
+/*
+ *
+ * Instituto de Matematica Pura e Aplicada - IMPA
+ * Departamento de Dinamica dos Fluidos
+ *
+ */
+
+package rpnumerics;
+
+
+//
+// finds a stationary point of the vector field f
+// using Newton method with the starting point x0
+//
+// fills the local information of eigenvalues, eigenvectors, schur decompositions
+// the point initial_ is taken as an initial guess.
+//
+public class StationaryPointCalc implements RpCalculation {
+    //
+    // Constants
+    //
+    
+    //
+    // Members
+    //
+    private PhasePoint initial_;
+    
+    //
+    // Constructors
+    //
+    public StationaryPointCalc(PhasePoint initial) {
+        initial_ = initial;
+    }
+    
+    //
+    // Accessors/Mutators
+    //
+    public PhasePoint getInitPoint() { return initial_; }
+    
+    public void setInitPoint(PhasePoint initial) { initial_ = initial; }
+    
+    //
+    // Methods
+    //
+    public   RpSolution recalc() throws RpException{
+        return calc();
+        
+        
+    }
+    
+    public   RpSolution calc() throws RpException{
+    
+        
+        return calc(initial_);
+            
+  
+    }
+    
+    
+    private native RpSolution calc(PhasePoint initialPoint);
+    
+}
