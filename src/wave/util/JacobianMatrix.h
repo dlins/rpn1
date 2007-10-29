129ffb2b00aeec7e74873a71c8e6ce06e490e425
daniel <daniel@9db63477-e02e-0410-9af5-e195358994a9>
2007-10-29 19:08:01 +0000

    git-svn-id: svn://svn.impa.br/rpn/trunk@209 9db63477-e02e-0410-9af5-e195358994a9

--- src/wave/util/JacobianMatrix.h
+++ src/wave/util/JacobianMatrix.h
@@ -1,21 +1,112 @@
+/*
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) JacobianMatrix.h
+ */
 
-#ifndef _Jacobian_H
-#define	_Jacobian_H
+#ifndef _JacobianMatrix_H
+#define _JacobianMatrix_H
 
-#include "JetMatrix.h"
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+#include "Vector.h"
+#include "except.h"
+
+/*
+ * ---------------------------------------------------------------
+ * Definitions:
+ */
 
 
+class JacobianMatrix
+{
+private:
+	int n_comps_, size_;
+	Vector v_;
 
-class JacobianMatrix : public JetMatrix{
-    
-    //! the value accessor at vindx index
-    void setVal(int vindx [], double val);
-    //! the value mutator at vindx index
-    double getVal(int vindex []) const;
-    
-    
+	class RangeViolation : public exception { };
+public:
+	JacobianMatrix(void);
+	JacobianMatrix(const int n_comps);
+	JacobianMatrix(const JacobianMatrix & jacobianMatrix);
+
+	int n_comps(void) const;
+	void resize(int n_comps);
+	void range_check(int comp) const;
+	JacobianMatrix &zero(void);
+
+	double * operator()(void);
+	double operator()(int i, int j);
+	void operator()(int i, int j, double value);
+
 };
 
+inline JacobianMatrix::JacobianMatrix(void) : 
+	n_comps_(1),
+	size_(1),
+	v_(1)
+{
+}
 
-#endif	/* _Jacobian_H */
+inline JacobianMatrix::JacobianMatrix(const int n_comps) : 
+	n_comps_(n_comps),
+	size_(n_comps * n_comps),
+	v_(size_)
+{
+}
 
+inline JacobianMatrix::JacobianMatrix(const JacobianMatrix & jacobianMatrix) : 
+	n_comps_(jacobianMatrix.n_comps_),
+	size_(n_comps_ * n_comps_),
+	v_(jacobianMatrix.v_)
+{
+}
+
+inline int JacobianMatrix::n_comps(void) const
+{
+	return n_comps_;
+}
+
+inline void JacobianMatrix::resize(int n_comps)
+{
+	v_.resize(n_comps * n_comps);
+	n_comps_ = n_comps;
+}
+
+inline void JacobianMatrix::range_check(int comp) const
+{
+	if (comp < 0 || comp >= n_comps())
+		THROW(JacobianMatrix::RangeViolation());
+}
+
+inline JacobianMatrix & JacobianMatrix::zero(void)
+{
+	v_.zero();
+	return *this;
+}
+
+inline double * JacobianMatrix::operator()(void)
+{
+	return v_.components();
+}
+
+inline double JacobianMatrix::operator()(int i, int j)
+{
+	range_check(i);
+	range_check(j);
+	return v_.component(i * n_comps_ + j);
+}
+
+inline void JacobianMatrix::operator()(int i, int j, double value)
+{
+	range_check(i);
+	range_check(j);
+	double * value_ = & v_.component(i * n_comps_ + j);
+	*value_ = value;
+}
+
+#endif //! _JacobianMatrix_H
