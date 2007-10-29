6065a82cde663787bb9126757be767302375a09e
daniel <daniel@9db63477-e02e-0410-9af5-e195358994a9>
2007-10-29 19:11:11 +0000

    git-svn-id: svn://svn.impa.br/rpn/trunk@211 9db63477-e02e-0410-9af5-e195358994a9

--- src/wave/util/HessianMatrix.h
+++ src/wave/util/HessianMatrix.h
@@ -1,33 +1,114 @@
-/* IMPA - Fluid Dynamics Laboratory
+/*
+ * IMPA - Fluid Dynamics Laboratory
  *
  * RPn Project
  *
  * @(#) HessianMatrix.h
- *
  */
 
-#ifndef HessianMatrix_H
-#define	HessianMatrix_H
+#ifndef _HessianMatrix_H
+#define _HessianMatrix_H
 
-#include "JetMatrix.h"
-#include "Vector2.h"
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+#include "Vector.h"
+#include "except.h"
+
+/*
+ * ---------------------------------------------------------------
+ * Definitions:
+ */
 
 
+class HessianMatrix
+{
+private:
+	int n_comps_, size_;
+	Vector v_;
 
-class HessianMatrix{//: public Matrix2,public JetMatrix {
-    
-public :
-    //!Constructor
-    HessianMatrix(const int n);
-    virtual ~HessianMatrix();
-    
-    //! the value accessor at vindx index
-    void setVal(int vindx [], double val);
-    //! the value mutator at vindx index
-    double getVal(int vindex []) const;
+	class RangeViolation : public exception { };
+public:
+	HessianMatrix(void);
+	HessianMatrix(const int n_comps);
+	HessianMatrix(const HessianMatrix & hessianMatrix);
+
+	int n_comps(void) const;
+	void resize(int n_comps);
+	void range_check(int comp) const;
+	HessianMatrix &zero(void);
+
+	double * operator()(void);
+	double operator()(int i, int j, int k);
+	void operator()(int i, int j, int k, double value);
+
 };
 
+inline HessianMatrix::HessianMatrix(void) : 
+	n_comps_(1),
+	size_(1),
+	v_(1)
+{
+}
 
+inline HessianMatrix::HessianMatrix(const int n_comps) : 
+	n_comps_(n_comps),
+	size_(n_comps * n_comps * n_comps),
+	v_(size_)
+{
+}
 
-#endif	/* _HessianMatrix_H */
+inline HessianMatrix::HessianMatrix(const HessianMatrix & hessianMatrix) : 
+	n_comps_(hessianMatrix.n_comps_),
+	size_(n_comps_ * n_comps_ * n_comps_),
+	v_(hessianMatrix.v_)
+{
+}
 
+inline int HessianMatrix::n_comps(void) const
+{
+	return n_comps_;
+}
+
+inline void HessianMatrix::resize(int n_comps)
+{
+	v_.resize(n_comps * n_comps * n_comps);
+	n_comps_ = n_comps;
+}
+
+inline void HessianMatrix::range_check(int comp) const
+{
+	if (comp < 0 || comp >= n_comps())
+		THROW(HessianMatrix::RangeViolation());
+}
+
+inline HessianMatrix & HessianMatrix::zero(void)
+{
+	v_.zero();
+	return *this;
+}
+
+inline double * HessianMatrix::operator()(void)
+{
+	return v_.components();
+}
+
+inline double HessianMatrix::operator()(int i, int j, int k)
+{
+	range_check(i);
+	range_check(j);
+	range_check(k);
+	return v_.component(i * n_comps_ * n_comps_ + j * n_comps_ + k);
+}
+
+inline void HessianMatrix::operator()(int i, int j, int k, double value)
+{
+	range_check(i);
+	range_check(j);
+	range_check(k);
+	double * value_ = & v_.component(i * n_comps_ * n_comps_ + j * n_comps_ + k);
+	*value_ = value;
+}
+
+#endif //! _HessianMatrix_H
