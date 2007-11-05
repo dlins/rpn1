d49d1fca51a016f837c696bc0d5ee963914ca209
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-05 17:07:42 +0000

    ""
    git-svn-id: svn://svn.impa.br/rpn/trunk@221 9db63477-e02e-0410-9af5-e195358994a9

--- src/wave/util/JetMatrix.h
+++ src/wave/util/JetMatrix.h
@@ -27,13 +27,41 @@ class JetMatrix
 private:
 	int n_comps_, size_;
 	Vector v_;
-
+private:
+    int n_comps_, size_;
+    Vector v_;
+    
+    class RangeViolation : public exception { };
+    
+    
 	class RangeViolation : public exception { };
 public:
-	JetMatrix(void);
-	JetMatrix(const int n_comps);
-	JetMatrix(const JetMatrix & jetMatrix);
-
+    JetMatrix(void);
+    JetMatrix(const int n_comps);
+    JetMatrix(const JetMatrix & jetMatrix);
+    
+    int n_comps(void) const;
+    void resize(int n_comps);
+    void range_check(int comp) const;
+    JetMatrix &zero(void);
+    
+    
+    double operator()(int i);
+    double operator()(int i, int j);
+    double operator()(int i, int j, int k);
+    
+    void operator()(int i, double value);
+    void operator()(int i, int j, double value);
+    void operator()(int i, int j, int k, double value);
+    
+    
+private:
+    
+    bool c0_, c1_, c2_;
+    
+protected:
+    
+    double * operator()(void);
 	int n_comps(void) const;
 	void resize(int n_comps);
 	void range_check(int comp) const;
@@ -50,98 +78,87 @@ public:
 
 };
 
-inline JetMatrix::JetMatrix(void) : 
-	n_comps_(1),
-	size_(3),
-	v_(size_)
-{
+inline JetMatrix::JetMatrix(void) : n_comps_(1), size_(3), v_(size_), c0_(false), c1_(false), c2_(false) {
 }
 
-inline JetMatrix::JetMatrix(const int n_comps) : 
-	n_comps_(n_comps),
-	size_(n_comps * (1 + n_comps * (1 + n_comps))),
-	v_(size_)
-{
+inline JetMatrix::JetMatrix(const int n_comps) : n_comps_(n_comps), size_(n_comps * (1 + n_comps * (1 + n_comps))),
+        v_(size_), c0_(false), c1_(false), c2_(false) {
 }
 
-inline JetMatrix::JetMatrix(const JetMatrix & jetMatrix) : 
-	n_comps_(jetMatrix.n_comps_),
-	size_(n_comps_ * (1 + n_comps_ * (1 + n_comps_))),
-	v_(jetMatrix.v_)
-{
+inline JetMatrix::JetMatrix(const JetMatrix & jetMatrix) :n_comps_(jetMatrix.n_comps_), size_(n_comps_ * (1 + n_comps_ * (1 + n_comps_))),
+        v_(jetMatrix.v_), c0_(false), c1_(false), c2_(false) {
 }
 
-inline int JetMatrix::n_comps(void) const
-{
-	return n_comps_;
+inline int JetMatrix::n_comps(void) const {
+    return n_comps_;
 }
 
-inline void JetMatrix::resize(int n_comps)
-{
-	v_.resize(n_comps * (1 + n_comps * (1 + n_comps)));
-	n_comps_ = n_comps;
+inline void JetMatrix::resize(int n_comps) {
+    v_.resize(n_comps * (1 + n_comps * (1 + n_comps)));
+    n_comps_ = n_comps;
 }
 
-inline void JetMatrix::range_check(int comp) const
-{
-	if (comp < 0 || comp >= n_comps())
-		THROW(JetMatrix::RangeViolation());
+inline void JetMatrix::range_check(int comp) const {
+    if (comp < 0 || comp >= n_comps())
+        THROW(JetMatrix::RangeViolation());
 }
 
-inline JetMatrix & JetMatrix::zero(void)
-{
-	v_.zero();
-	return *this;
+inline JetMatrix & JetMatrix::zero(void) {
+    v_.zero();
+    return *this;
 }
 
-inline double * JetMatrix::operator()(void)
-{
-	return v_.components();
+inline double * JetMatrix::operator()(void) {
+    return v_.components();
 }
 
-inline double JetMatrix::operator()(int i)
-{
-	range_check(i);
-	return v_.component(i);
+inline double JetMatrix::operator()(int i) {
+    range_check(i);
+    if (!c0_)
+        THROW(JetMatrix::RangeViolation());
+    return v_.component(i);
 }
 
-inline double JetMatrix::operator()(int i, int j)
-{
-	range_check(i);
-	range_check(j);
-	return v_.component((n_comps_) + (i*n_comps_ + j));
+inline double JetMatrix::operator()(int i, int j) {
+    range_check(i);
+    range_check(j);
+    if (!c1_)
+        THROW(JetMatrix::RangeViolation());
+    return v_.component((n_comps_) + (i*n_comps_ + j));
 }
 
-inline double JetMatrix::operator()(int i, int j, int k)
-{
-	range_check(i);
-	range_check(j);
-	range_check(k);
-	return v_.component((n_comps_ * (1 + n_comps_)) + (i*n_comps_*n_comps_ + j*n_comps_ + k));
+inline double JetMatrix::operator()(int i, int j, int k) {
+    range_check(i);
+    range_check(j);
+    range_check(k);
+    if (!c2_)
+       THROW(JetMatrix::RangeViolation());
+    return v_.component((n_comps_ * (1 + n_comps_)) + (i*n_comps_*n_comps_ + j*n_comps_ + k));
 }
 
-inline void JetMatrix::operator()(int i, double value)
-{
-	range_check(i);
-	double * value_ = & v_.component(i);
-	*value_ = value;
+inline void JetMatrix::operator()(int i, double value) {
+    range_check(i);
+    c0_=true;
+    double * value_ = & v_.component(i);
+    *value_ = value;
 }
 
-inline void JetMatrix::operator()(int i, int j, double value)
-{
-	range_check(i);
-	range_check(j);
-	double * value_ = & v_.component((n_comps_) + (i*n_comps_ + j));
-	*value_ = value;
+inline void JetMatrix::operator()(int i, int j, double value) {
+    range_check(i);
+    range_check(j);
+    c1_=true;
+
+    double * value_ = & v_.component((n_comps_) + (i*n_comps_ + j));
+    *value_ = value;
 }
 
-inline void JetMatrix::operator()(int i, int j, int k, double value)
-{
-	range_check(i);
-	range_check(j);
-	range_check(k);
-	double * value_ = & v_.component((n_comps_ * (1 + n_comps_)) + (i*n_comps_*n_comps_ + j*n_comps_ + k));
-	*value_ = value;
+inline void JetMatrix::operator()(int i, int j, int k, double value) {
+    range_check(i);
+    range_check(j);
+    range_check(k);
+    c2_=true;
+    double * value_ = & v_.component((n_comps_ * (1 + n_comps_)) + (i*n_comps_*n_comps_ + j*n_comps_ + k));
+    *value_ = value;
 }
 
 #endif //! _JetMatrix_H
