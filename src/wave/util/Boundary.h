1ddea73a98518dcabc29b820130e448a2e0f3a23
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-05 17:07:34 +0000

    ""
    git-svn-id: svn://svn.impa.br/rpn/trunk@220 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/wave/util/Boundary.h
@@ -0,0 +1,50 @@
+/**
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) Boundary.h
+ **/
+
+#ifndef _Boundary_H
+#define	_Boundary_H
+
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+#include "RealVector.h"
+
+//! Definition of class Boundary.
+/*!
+
+ TODO:
+ NOTE :
+
+ @ingroup rpnumerics
+ */
+
+
+class Boundary {
+    
+public:
+
+    virtual ~Boundary();
+
+    //! Check if the point is inside the boundary.
+    virtual bool inside(const RealVector &y) const = 0;
+    
+    //! Minimums boundary values accessor 
+    virtual const  RealVector minimums() const = 0;
+    
+    //! Maximums boundary values accessor
+    virtual const  RealVector maximums() const = 0;
+    
+    virtual const  RealVector intersect(RealVector &y1, RealVector &y2) const = 0;
+};
+
+inline Boundary::~Boundary() { };
+
+
+#endif	/* _Boundary_H */
+
