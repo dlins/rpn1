ec61dc87d3a81c8375b2f9887949ca83981197a2
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-05 17:08:20 +0000

    ""
    git-svn-id: svn://svn.impa.br/rpn/trunk@224 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/wave/util/RectBoundary.cc
@@ -0,0 +1,19 @@
+/*
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) RectBoundary.cc
+ */
+
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+#include "RectBoundary.h"
+
+/*
+ * ---------------------------------------------------------------
+ * Definitions:
+ */
+
