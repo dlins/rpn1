85689f8329518da9b3a007ea6d2bcff22ffffb5a
daniel <daniel@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-05 20:43:01 +0000

    Daniel
    git-svn-id: svn://svn.impa.br/rpn/trunk@244 9db63477-e02e-0410-9af5-e195358994a9

--- src/rpnumerics/WaveFlow.h
+++ src/rpnumerics/WaveFlow.h
@@ -1,4 +1,4 @@
-/**A
+/**
  * IMPA - Fluid Dynamics Laboratory
  *
  * RPn Project
@@ -11,10 +11,7 @@
 #define	_WaveFlow_H
 
 #include "RpFunction.h"
-<<<<<<< .mine
 #include "PhasePoint.h"
-=======
->>>>>>> .r236
 
 //! Definition of class WaveFlow.
 /*!
@@ -27,29 +24,17 @@
 
 class WaveFlow : public RpFunction {
     
-<<<<<<< .mine
 public:
 	~WaveFlow(void);
-=======
-    
-      PhasePoint getXZero();
->>>>>>> .r236
 
-<<<<<<< .mine
 	virtual const PhasePoint XZero(void) const = 0;
 	virtual void XZero(const PhasePoint & xzero) const = 0;
-=======
-    void setXZero (PhasePoint  & xzero);
->>>>>>> .r236
 
 };
 
-<<<<<<< .mine
 inline WaveFlow::~WaveFlow(void)
 {
 }
 
-=======
->>>>>>> .r236
 #endif	/* _WaveFlow_H */
 
