757f96948856a92d29c604cef2f058ea67335b87
daniel <daniel@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-06 17:51:11 +0000

    Just organizing...
    Daniel
    
    
    
    git-svn-id: svn://svn.impa.br/rpn/trunk@254 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/rpnumerics/Physics.h
@@ -0,0 +1,52 @@
+/**
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) Physics.h
+ **/
+
+#ifndef _Physics_H
+#define	_Physics_H
+
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+//#include "AccumulationFunction.h"
+#include "Boundary.h"
+#include "FluxFunction.h"
+#include "Space.h"
+#include "WaveFlow.h"
+
+//! Definition of class Physics.
+/*!
+
+ TODO:
+ NOTE :
+
+ @ingroup rpnumerics
+ */
+
+class Physics {
+    
+public :
+	virtual ~Physics();
+        
+        //virtual const AccumulationFunction & accumulation() const = 0;
+
+        virtual const Boundary & boundary() const= 0;
+
+	virtual void boundary(const Boundary & boundary) = 0;
+        
+        virtual const FluxFunction & fluxFunction() const = 0;
+
+        virtual const Space & domain() const = 0;
+
+	virtual const WaveFlow & flow() const = 0;
+
+};
+
+inline Physics::~Physics() { };
+
+#endif	//! _Physics_H
