083d3766f5dc4508a75637a1ad7ea9f9b73c952c
daniel <daniel@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-06 17:52:46 +0000

    git-svn-id: svn://svn.impa.br/rpn/trunk@255 9db63477-e02e-0410-9af5-e195358994a9

---
+++ bin/Template.cc
@@ -0,0 +1,22 @@
+/*
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) ClassName.cc
+ */
+
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+#include "ClassName.h"
+
+/*
+ * ---------------------------------------------------------------
+ * Definitions:
+ */
+#define RP_... 1
+
+//! Code comes here! daniel@impa.br
+
---
+++ bin/Template.h
@@ -0,0 +1,32 @@
+/*
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) ClassName.h
+ */
+
+#ifndef _ClassName_H
+#define _ClassName_H
+
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+#include ""
+
+/*
+ * ---------------------------------------------------------------
+ * Definitions:
+ */
+#define RP_... 1
+
+class ClassName {
+
+private:
+
+public:
+
+};
+
+#endif //! _ClassName_H
