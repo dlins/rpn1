9441ce96acd6ec7cf384112178a8ef8e3d8ec3bd
daniel <daniel@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-06 19:25:23 +0000

    Just re-organizing.
    Daniel.
    
    
    
    git-svn-id: svn://svn.impa.br/rpn/trunk@259 9db63477-e02e-0410-9af5-e195358994a9

--- src/rpnumerics/FluxParams.h
+++ src/rpnumerics/FluxParams.h
@@ -3,53 +3,122 @@
  * RPn Project
  *
  * @(#) FluxParams.h
- **/
+ */
 
 #ifndef _FluxParams_H
 #define	_FluxParams_H
+
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
 #include "RealVector.h"
 
-class FluxParams{
-    public:
-        
-        FluxParams(const char *, const RealVector &, const int);
-        
-        
-        //! Individual flux parameters  accessor
-        const double  getElement(const int ) const ;
-        //! Complete flux parameters  accessor
-        const  RealVector & getParams() const ;
-        //! Flux parameters  mutator
-        void setParams(const double * params);
-        //! Flux parameters mutator
-        void setParams(const RealVector params);
-        //! Flux parameters mutator
-        void setParam(const int , const double );
-        //! Physics ID accessor
-        const char * getPhysicsID() const;
-        //! Default flux parameters accessor
-        virtual  FluxParams defaultParams();
-        
-        private:
-            
-            RealVector params_;
-            
-            char *physicsID_;
-            
+/*
+ * ---------------------------------------------------------------
+ * Definitions:
+ */
+
+
+class FluxParams {
+
+private:
+	RealVector params_;
+	RealVector initParams_;
+
+public:
+	FluxParams(void);
+	FluxParams(const int size, const double *coords);
+	FluxParams(const RealVector & params);
+	FluxParams(const FluxParams & params);
+	virtual ~FluxParams(void);
+
+	const RealVector &params(void);
+	const RealVector &operator()(void);
+	const RealVector operator()(void) const;
+
+	void params(int size, double * coords);
+	void params(const RealVector & params);
+
+	double component(int index);
+	void component(int index, double value);
+
+	//virtual FluxParams defaultParams(void) const = 0;
+
+	bool operator==(const FluxParams & fluxParams);
+	void reset(void);
+
 };
 
+inline FluxParams::FluxParams(void) :
+	params_(),
+	initParams_()
+{
+}
 
-inline const RealVector & FluxParams::getParams(){ return params_;}
+inline FluxParams::FluxParams(const RealVector & params) :
+	params_(params),
+	initParams_(params)
+{
+}
 
-inline void FluxParams::setParams(const double * params){ params_= new RealVector(params);}
+inline FluxParams::FluxParams(const FluxParams & params) :
+	params_(params()),
+	initParams_(params())
+{
+}
 
-inline void FluxParams::setParams(const RealVector params){ params_=params;}
+inline FluxParams::~FluxParams()
+{
+}
 
-inline void FluxParams::setParam(const int index, const double value){params_(index)=value;}
+inline const RealVector & FluxParams::params(void)
+{
+	return params_;
+}
 
-inline double FluxParams::getElement(int index){ return params_(index);}
+inline const RealVector & FluxParams::operator()(void)
+{
+	return params_;
+}
 
-inline const char * FluxParams::getPhysicsID(){return physicsID_;}
+inline const RealVector FluxParams::operator()(void) const
+{
+	return params_;
+}
 
-#endif	/* _FluxParams_H */
+inline void FluxParams::params(int size, double * coords) 
+{ 
+	RealVector p_(size, coords);
+	params_ = p_;
+}
 
+inline void FluxParams::params(const RealVector & params)
+{
+	params_ = params;
+}
+
+inline double FluxParams::component(int index)
+{
+	return params_(index);
+}
+
+inline void FluxParams::component(int index, double value)
+{
+	params_(index) = value;
+}
+
+inline bool FluxParams::operator==(const FluxParams & fluxParams)
+{ 
+	if ( params_ == fluxParams() )
+		return true;	
+	else
+		return false;
+}
+
+inline void FluxParams::reset(void)
+{
+	params_ = initParams_;
+}
+
+#endif	//! _FluxParams_H
