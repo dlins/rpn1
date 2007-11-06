87fe78ef4e3ec0870f955702f5920aa3b8703886
daniel <daniel@9db63477-e02e-0410-9af5-e195358994a9>
2007-11-06 17:50:19 +0000

    Adding "State" classes.
    git-svn-id: svn://svn.impa.br/rpn/trunk@253 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/rpnumerics/physics/QuadWaveState.cc
@@ -0,0 +1,8 @@
+#include "QuadWaveState.h"
+
+QuadWaveState::QuadWaveState(const int dim):WaveState(dim){}
+
+QuadWaveState::QuadWaveState(const int dim,sigma sig):WaveState(dim),sigma_(sig){}
+
+QuadWaveState::~QuadWaveState(){}
+
---
+++ src/rpnumerics/physics/WaveState.cc
@@ -0,0 +1,8 @@
+#include "WaveState.h"
+
+WaveState::WaveState(const int dim ):coords_(new RealVector(dim)){}
+WaveState::~WaveState(){ delete coords_;}
+
+double WaveState::operator() (const int comp) const { return coords_->component(comp);}
+double & WaveState::operator () (const int comp)  {return coords_->component(comp);}
+
