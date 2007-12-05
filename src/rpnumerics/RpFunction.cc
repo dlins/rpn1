feeb743092b8dda0a430cc3bc8cd1e4c83fe626f
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2007-12-05 14:25:05 +0000

    ""
    git-svn-id: svn://svn.impa.br/rpn/trunk@288 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/rpnumerics/RpFunction.cc
@@ -0,0 +1,19 @@
+/**
+ * IMPA - Fluid Dynamics Laboratory
+ *
+ * RPn Project
+ *
+ * @(#) RpFunction.cc
+ **/
+
+/*
+ * ---------------------------------------------------------------
+ * Includes:
+ */
+#include "RpFunction.h"
+
+/*
+ * ---------------------------------------------------------------
+ * Definitions:
+ */
+RpFunction::~RpFunction(void){}
