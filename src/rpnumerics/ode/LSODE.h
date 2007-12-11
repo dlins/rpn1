dcd309b2fb5b88a766492e4f9adf5cc94e9e4b3f
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2007-12-11 17:12:20 +0000

    ""
    git-svn-id: svn://svn.impa.br/rpn/trunk@297 9db63477-e02e-0410-9af5-e195358994a9

--- src/rpnumerics/ode/LSODE.h
+++ src/rpnumerics/ode/LSODE.h
@@ -23,20 +23,17 @@
  */
 
 
-// extern "C"{
-//    
-//    lsode_(int (*)(int *, double *, double *, double *), int *, double *, double *, double *,
-//            int *, double *, double *, int *, int *, int *, double *, int *,
-//            int *, int *, int(*)(int *, double *, double *, int *, int *, double *, int *), int *, int*, double*);
-//    
-//    
-//}
-
+extern "C"{
+    
+    void  lsode_(int (*)(int *, double *, double *, double *), int *, double *, double *, double *,
+            int *, double *, double *, int *, int *, int *, double *, int *,
+            int *, int *, int(*)(int *, double *, double *, int *, int *, double *, int *), int *, int*, double*);
+}
 
 class LSODE:public ODESolver {
     
     
-   
+    
     
 private:
     
@@ -44,13 +41,12 @@ private:
     
     const RpFunction * rpFunction_;
     
-    int function (int *,double *,double *,double *);
+    int function(int *, double *, double *, double *);
     
 public:
     
     LSODE(const LSODEProfile &);
-     ~LSODE();
-    
+    ~LSODE();
     
     ODESolution & solve(const RealVector & , int ) const;
     const ODESolverProfile & getProfile() const ;
