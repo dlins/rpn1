b25fefe7a86a8453763a1179a597cf70895cdf18
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2008-04-22 19:14:05 +0000

    git-svn-id: svn://svn.impa.br/rpn/trunk@414 9db63477-e02e-0410-9af5-e195358994a9

--- src/build/linux_i686/bool.d
+++
@@ -1,1 +0,0 @@
-./build/linux_i686/bool.o ./../lib/linux_i686/librpnumerics.so(bool.o) build/linux_i686/bool.d: wave/util/bool.cc
--- src/build/linux_i686/lsode2.d
+++
@@ -1,1 +0,0 @@
-./build/linux_i686/lsode2.o ./../lib/linux_i686/librpnumerics.so(lsode2.o) build/linux_i686/lsode2.d: ./rpnumerics/ode/lsode2.F
