33b3c28e9abdfb5be7d12a9f7792ad0a91947f27
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2009-04-29 17:30:43 +0000

    Adicionando codigo do Carlos
    git-svn-id: svn://svn.impa.br/rpn/trunk@562 9db63477-e02e-0410-9af5-e195358994a9

---
+++ src/rpn/usecase/BifurcationPlotAgent.java
@@ -0,0 +1,33 @@
+package rpn.usecase;
+
+import rpn.component.*;
+import rpnumerics.*;
+import wave.util.*;
+
+public class BifurcationPlotAgent extends RpModelPlotAgent {
+
+	static public final String DESC_TEXT = "Bifurcation Curve";
+    
+    static private BifurcationPlotAgent instance_ = null;
+	
+	public BifurcationPlotAgent() {
+		super(DESC_TEXT, rpn.RPnConfigReader.HUGONIOT);
+	}
+
+	public RpGeometry createRpGeometry(RealVector[] coords) {
+		BifurcationCurveCalc bifurcationCurveCalc = RPNUMERICS.createBifurcationCalc();
+		
+		BifurcationCurveGeomFactory factory = new BifurcationCurveGeomFactory(bifurcationCurveCalc);
+		
+	    return factory.geom();
+	}
+	
+	 static public BifurcationPlotAgent instance() {
+	        if (instance_ == null) {
+	            instance_ = new BifurcationPlotAgent();
+	        }
+	        
+	        return instance_;
+	 }
+
+}
