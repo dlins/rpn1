b49e416d54adef873b3475772974d0aeb09c6730
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2009-07-23 19:04:55 +0000

    Alterando o layout do dialogo de configuração inicial (sem arquivo de entrada), e colocando a implementacao de  Quad2FluxFunction sem loop.
    git-svn-id: svn://svn.impa.br/rpn/trunk@626 9db63477-e02e-0410-9af5-e195358994a9

--- src/rpnumerics/physics/quad/Quad2FluxParams.cc
+++ src/rpnumerics/physics/quad/Quad2FluxParams.cc
@@ -6,7 +6,7 @@ const double Quad2FluxParams::DEFAULT_B[2][2] = { { 0., .1 }, { -.1, 0. } };
 const double Quad2FluxParams::DEFAULT_C[2][2][2] = { { { -1., 0. }, { 0., 1. } }, { { 0., 1. }, { 1., 0. } } };
 //
 
-Quad2FluxParams::Quad2FluxParams(void) :FluxParams(RealVector(14)) 
+Quad2FluxParams::Quad2FluxParams(void) :FluxParams(RealVector(10))
 {
     component(0, -1); //A1
 
@@ -27,18 +27,7 @@ Quad2FluxParams::Quad2FluxParams(void) :FluxParams(RealVector(14))
     component(8, -.1);//D2
 
     component(9, 0); //E2
-// 
-//    int m=2;
-//
-//        for (int i = 0; i < m; i++) {
-//            component(i, DEFAULT_A[i]);
-//            for (int j = 0; j < m; j++) {
-//            component(m + i * m + j, DEFAULT_B[i] [j]);
-//                for (int k = 0; k < m; k++) {
-//                    component(m + m * m + i * m * m + j * m + k, DEFAULT_C[i] [j] [k]);
-//                }
-//            }
-//        }
+
     
     
     
