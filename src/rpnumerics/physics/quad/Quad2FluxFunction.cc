8baeead5edf728b27416a67c051e129b42d10fbc
edsonlan <edsonlan@9db63477-e02e-0410-9af5-e195358994a9>
2009-09-28 19:57:14 +0000

    git-svn-id: svn://svn.impa.br/rpn/trunk@660 9db63477-e02e-0410-9af5-e195358994a9

--- src/rpnumerics/physics/quad/Quad2FluxFunction.cc
+++ src/rpnumerics/physics/quad/Quad2FluxFunction.cc
@@ -146,10 +146,15 @@ int Quad2FluxFunction::jet(const WaveState & x, JetMatrix & y, int degree = 2) c
 //    out1 = 0.5 * (a2 * pow(u, (double) 2) + 2.0 * b2 * u * v + c2 * pow(v, (double) 2)) + d2 * u + e2*v;
 //
 
-    out0 = 0.5 * ( (a1 * u * u) + (2.0 * b1 * u * v) + (c1 * v * v) ) + d1 * u + e1*v;
-
-    out1 = 0.5 * ( (a2 * u * u) + (2.0 * b2 * u * v) + (c2 * v * v) ) + d2 * u + e2*v;
+//    out0 = 0.5 * ( (a1 * u * u) + (2.0 * b1 * u * v) + (c1 * v * v) ) + d1 * u + e1*v;
+//
+//    out1 = 0.5 * ( (a2 * u * u) + (2.0 * b2 * u * v) + (c2 * v * v) ) + d2 * u + e2*v;
 //    
+
+
+    out0 = 3.0*u*u/2.0+v*v/2.0;
+
+    out1 = u*v;
 
 
 //    out0 = (a1 * u * u) + b1 * u * v + c1 * (v * v) + d1 * u + e1*v;
@@ -168,10 +173,19 @@ int Quad2FluxFunction::jet(const WaveState & x, JetMatrix & y, int degree = 2) c
         // Calculate DF
         double out00, out01, out10, out11;
 
-        out00 = a1 * u + b1 * v + d1;
-        out01 = b1 * u + c1 * v + e1;
-        out10 = a2 * u + b2 * v + d2;
-        out11 = b2 * u + c2 * v + e2;
+//        out00 = a1 * u + b1 * v + d1;
+//        out01 = b1 * u + c1 * v + e1;
+//        out10 = a2 * u + b2 * v + d2;
+//        out11 = b2 * u + c2 * v + e2;
+
+
+        out00 = 3.0*u;
+        out01 = v;
+        out10 = v;
+        out11 = u;
+
+
+
 
         y(0, 0, out00);
         y(0, 1, out01);
@@ -180,14 +194,26 @@ int Quad2FluxFunction::jet(const WaveState & x, JetMatrix & y, int degree = 2) c
 
         if (degree > 1) {
             // Calculate D2F
-            y(0, 0, 0, a1);
-            y(1, 0, 0, b1);
-            y(0, 1, 0, b1);
-            y(1, 1, 0, c1);
-            y(0, 0, 1, a2);
-            y(1, 0, 1, b2);
-            y(0, 1, 1, b2);
-            y(1, 1, 1, c2);
+//            y(0, 0, 0, a1);
+//            y(1, 0, 0, b1);
+//            y(0, 1, 0, b1);
+//            y(1, 1, 0, c1);
+//            y(0, 0, 1, a2);
+//            y(1, 0, 1, b2);
+//            y(0, 1, 1, b2);
+//            y(1, 1, 1, c2);
+
+            y(0, 0, 0, 3.0);
+            y(1, 0, 0, 0.0);
+            y(0, 1, 0, 0.0);
+            y(1, 1, 0, 1.0);
+            y(0, 0, 1, 0.0);
+            y(1, 0, 1, 1.0);
+            y(0, 1, 1, 1.0);
+            y(1, 1, 1, 0.0);
+
+
+
 
             if (degree > 2) {
                 return 0; //UNSUCCESSFUL_PROCEDURE;
