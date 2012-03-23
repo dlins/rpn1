afaf9746fb9d607d7047a5d69d9baac2802a9202
Edson Landim de Almeida <edsonlan@impa.br>
2012-03-23 17:42:23 -0300

    Versao do Eigen Contour que usa GridValues

--- src/c++/rpnumerics/Eigenvalue_Contour.cpp
+++ src/c++/rpnumerics/Eigenvalue_Contour.cpp
@@ -1,104 +1,7 @@
 #include "Eigenvalue_Contour.h"
 
-void Eigenvalue_Contour::fill_with_jet(const RpFunction *flux_object, int n, double *in, int degree, double *F, double *J, double *H) {
-    RealVector r(n);
-    double *rp = r;
-    for (int i = 0; i < n; i++) rp[i] = in[i];
-
-    // Will this work? There is a const somewhere in fluxParams.
-    //FluxParams fp(r);
-    //flux_object->fluxParams(FluxParams(r)); // flux_object->fluxParams(fp);
-
-    WaveState state_c(r);
-    JetMatrix c_jet(n);
-
-    flux_object->jet(state_c, c_jet, degree);
-
-    // Fill F
-    if (F != 0) for (int i = 0; i < n; i++) F[i] = c_jet(i);
-
-    // Fill J
-    if (J != 0) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                J[i * n + j] = c_jet(i, j);
-            }
-        }
-    }
-
-    // Fill H
-    if (H != 0) {
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                for (int k = 0; k < n; k++) {
-                    H[(i * n + j) * n + k] = c_jet(i, j, k); 
-                }
-            }
-        }
-    }
-
-    return;
-}
-
-Eigenvalue_Contour::Eigenvalue_Contour(const FluxFunction *f, const AccumulationFunction *a, 
-                                       const RealVector &min, const RealVector &max, 
-                                       const int *cells, const Boundary *b){
-
-    int dim = 2;
-
-    ff = f; aa = a;
-    boundary = b;
-
-    double JF[4];
-    double JG[4];
-
-    // Create the grid...
-    //
-    pmin.resize(dim); pmax.resize(dim);
-
-    number_of_cells = new int[2];
-    for (int i = 0; i < 2; i++){
-        number_of_cells[i] = cells[i];
-        pmin.component(i) = min.component(i);
-        pmax.component(i) = max.component(i);
-    }
-
-    double delta[2];
-    for (int i = 0; i < pmin.size(); i++) delta[i] = (fabs(pmax.component(i) - pmin.component(i)))/(double)(number_of_cells[i]);
-
-    grid.resize(number_of_cells[0] + 1, number_of_cells[1] + 1);
-    eigenvalues_on_grid.resize(number_of_cells[0] + 1, number_of_cells[1] + 1);
-    eigenvalues_are_real_on_grid.resize(number_of_cells[0] + 1, number_of_cells[1] + 1);
-    vertex_in_domain.resize(number_of_cells[0] + 1, number_of_cells[1] + 1);
-
-    for (int i = 0; i <= number_of_cells[0]; i++){
-        for (int j = 0; j <= number_of_cells[1]; j++){
-            grid(i, j).resize(dim);
-            grid(i, j).component(0) = pmin.component(0) + (double)i*delta[0];
-            grid(i, j).component(1) = pmin.component(1) + (double)j*delta[1];
-
-            if (!boundary->inside(grid(i, j))){
-                vertex_in_domain(i, j) = false;
-                continue;
-            }
-            else vertex_in_domain(i, j) = true;
-
-            // Fill the eigenvalues
-            eigenvalues_on_grid(i, j).resize(dim);
-
-            fill_with_jet((RpFunction*)ff, dim, grid(i, j).components(), 1, 0, JF, 0);
-            fill_with_jet((RpFunction*)aa, dim, grid(i, j).components(), 1, 0, JG, 0);
-
-            std::vector<eigenpair> e;
-            Eigen::eig(dim, JF, JG, e);
-            for (int k = 0; k < e.size(); k++) eigenvalues_on_grid(i, j).component(k) = e[k].r;
-
-            // Are they real?
-            eigenvalues_are_real_on_grid(i, j).resize(dim);
-            for (int k = 0; k < e.size(); k++) eigenvalues_are_real_on_grid(i, j)[k] = (fabs(e[k].i) < 1e-10);
-        }
-    }
-
+Eigenvalue_Contour::Eigenvalue_Contour(){
+    gv = 0;
 
     levels.push_back(0.0); // At least this level can be contoured.
     family = 0;            // At least for this family.
@@ -107,21 +10,21 @@ Eigenvalue_Contour::Eigenvalue_Contour(const FluxFunction *f, const Accumulation
     family = 0;
 }
 
-Eigenvalue_Contour::~Eigenvalue_Contour(){
-    delete [] number_of_cells;
-}
-
 // Find the minimum and maximum lambdas, as were computed on the grid.
 //
 void Eigenvalue_Contour::find_minmax_lambdas(int f, double &min, double &max){
-    int r = eigenvalues_on_grid.rows();
-    int c = eigenvalues_on_grid.cols();
+    if (gv == 0) return;
 
-    min = max = eigenvalues_on_grid(0).component(f);
+    int r = gv->grid.rows();
+    int c = gv->grid.cols();
+
+    min = max = gv->e(0)[f].r;
 
     for (int i = 1; i < r*c; i++){
-        if (eigenvalues_on_grid(i).component(f) > max) max = eigenvalues_on_grid(i).component(f);
-        if (eigenvalues_on_grid(i).component(f) < min) min = eigenvalues_on_grid(i).component(f);
+        if (gv->point_inside(i)){
+            if (gv->e(i)[f].r > max) max = gv->e(i)[f].r;
+            if (gv->e(i)[f].r < min) min = gv->e(i)[f].r;
+        }
     }
 
     return;
@@ -131,6 +34,8 @@ void Eigenvalue_Contour::find_minmax_lambdas(int f, double &min, double &max){
 // The levels will be sorted from minimum to maximum.
 //
 void Eigenvalue_Contour::set_levels(int f, const std::vector<double> &l){
+    if (gv == 0) return;
+
     family = f;
 
     levels.clear();
@@ -147,6 +52,8 @@ void Eigenvalue_Contour::set_levels(int f, const std::vector<double> &l){
 // uniform separation between levels.
 //
 void Eigenvalue_Contour::set_levels_from_delta(int f, double delta_l){
+    if (gv == 0) return;
+
     family = f;
     levels.clear();
 
@@ -165,6 +72,8 @@ void Eigenvalue_Contour::set_levels_from_delta(int f, double delta_l){
 // Set the number of levels to be uniformly distributed.
 //
 void Eigenvalue_Contour::set_number_levels(int f, int n){
+    if (gv == 0) return;
+
     family = f;
     levels.clear();
 
@@ -183,15 +92,19 @@ void Eigenvalue_Contour::set_number_levels(int f, int n){
 // Set the levels radiating from the level at the given point, 
 // with the given distance between levels.
 //
-void Eigenvalue_Contour::set_levels_from_point(int f, const RealVector &p, double delta_l){
-    family = f;
+void Eigenvalue_Contour::set_levels_from_point(const FluxFunction *f, const AccumulationFunction *a, GridValues &g,
+                                               int fam, const RealVector &p, double delta_l){
+    g.fill_eigenpairs_on_grid(f, a);
+    gv = &g;
+
+    family = fam;
 
     levels.clear();
 
     double JF[4], JG[4];
 
-    fill_with_jet((RpFunction*)ff, p.size(), ((RealVector)p).components(), 1, 0, JF, 0);
-    fill_with_jet((RpFunction*)aa, p.size(), ((RealVector)p).components(), 1, 0, JG, 0);
+    f->fill_with_jet(p.size(), ((RealVector)p).components(), 1, 0, JF, 0);
+    a->fill_with_jet(p.size(), ((RealVector)p).components(), 1, 0, JG, 0);
 
     std::vector<eigenpair> e;
     Eigen::eig(p.size(), JF, JG, e);
@@ -200,7 +113,7 @@ void Eigenvalue_Contour::set_levels_from_point(int f, const RealVector &p, doubl
     double now_level = level - delta_l;
 
     double min_lambda, max_lambda;
-    find_minmax_lambdas(family, min_lambda, max_lambda);   
+    find_minmax_lambdas(family, min_lambda, max_lambda);
 
     // Downwards...
     while (now_level >= min_lambda){
@@ -216,7 +129,7 @@ void Eigenvalue_Contour::set_levels_from_point(int f, const RealVector &p, doubl
         now_level += delta_l;
     }
 
-    std::sort(levels.begin(), levels.end());
+    std::sort(levels.begin(), levels.end()); printf("levels.size() = %d\n", levels.size());
 
     return;
 }
@@ -232,16 +145,14 @@ void Eigenvalue_Contour::set_level(double l, int f){
 
 // Set the level at the given point.
 //
-void Eigenvalue_Contour::set_level_from_point(int f, const RealVector &p){
-//    set_family(f);
-    family = f;
-
-//    levels.clear();
+void Eigenvalue_Contour::set_level_from_point(const FluxFunction *f, const AccumulationFunction *a,
+                                              int fam, const RealVector &p){
+    family = fam;
 
     double JF[4], JG[4];
 
-    fill_with_jet((RpFunction*)ff, p.size(), ((RealVector)p).components(), 1, 0, JF, 0);
-    fill_with_jet((RpFunction*)aa, p.size(), ((RealVector)p).components(), 1, 0, JG, 0);
+    f->fill_with_jet(p.size(), ((RealVector)p).components(), 1, 0, JF, 0);
+    a->fill_with_jet(p.size(), ((RealVector)p).components(), 1, 0, JG, 0);
 
     std::vector<eigenpair> e;
     Eigen::eig(p.size(), JF, JG, e);
@@ -251,51 +162,45 @@ void Eigenvalue_Contour::set_level_from_point(int f, const RealVector &p){
     return;
 }
 
-//void Eigenvalue_Contour::set_family(int f){
-//    family = f;
-//    return;
-//}
-
 // Foncub.
 //
-int Eigenvalue_Contour::function_on_square(double *foncub, int i, int j, int is_square){
-    if (!vertex_in_domain(i + 1, j + 0)) return 0;
-    if (!eigenvalues_are_real_on_grid(i + 1, j + 0)[family]) return 0;
-    foncub[0] = eigenvalues_on_grid(i + 1, j + 0).component(family) - level;
+int Eigenvalue_Contour::function_on_square(double *foncub, int i, int j){
+    int is_square = gv->cell_type(i, j);
 
-    if (!vertex_in_domain(i + 0, j + 0)) return 0;
-    if (!eigenvalues_are_real_on_grid(i + 0, j + 0)[family]) return 0;
-    foncub[1] = eigenvalues_on_grid(i + 0, j + 0).component(family) - level;
+    if (!gv->eig_is_real(i + 1, j + 0)[family]) return 0;
+    else foncub[0] = gv->e(i + 1, j + 0)[family].r - level;
 
-    if (!vertex_in_domain(i + 1, j + 1)) return 0;
-    if (!eigenvalues_are_real_on_grid(i + 1, j + 1)[family]) return 0;
-    foncub[2] = eigenvalues_on_grid(i + 1, j + 1).component(family) - level;
+    if (!gv->eig_is_real(i + 0, j + 0)[family]) return 0;
+    else foncub[1] = gv->e(i + 0, j + 0)[family].r - level;
 
-    if (!vertex_in_domain(i + 0, j + 1)) return 0;
-    if (!eigenvalues_are_real_on_grid(i + 0, j + 1)[family]) return 0;
-    foncub[3] = eigenvalues_on_grid(i + 0, j + 1).component(family) - level;
+    if (!gv->eig_is_real(i + 0, j + 1)[family]) return 0;
+    else foncub[3] = gv->e(i + 0, j + 1)[family].r - level;
+
+    if (is_square == CELL_IS_SQUARE){
+        if (!gv->eig_is_real(i + 1, j + 1)[family]) return 0;
+        else foncub[2] = gv->e(i + 1, j + 1)[family].r - level;
+    }
 
     return 1;
 }
 
-int Eigenvalue_Contour::curve(std::vector< std::vector<RealVector> > &eigenvalues_curves,
+int Eigenvalue_Contour::curve(const FluxFunction *f, const AccumulationFunction *a, 
+                              GridValues &g, 
+                              std::vector< std::vector<RealVector> > &eigenvalues_curves,
                               std::vector<double> &out_levels){
+
+    g.fill_eigenpairs_on_grid(f, a);
+    gv = &g;
 
     eigenvalues_curves.clear();
     out_levels.clear();
 
     int info;
 
-    double rect[4];
-    rect[0] = pmin.component(0);
-    rect[1] = pmax.component(0);
-    rect[2] = pmin.component(1);
-    rect[3] = pmax.component(1);
-
     for (int i = 0; i < levels.size(); i++){
         level = levels[i];
         std::vector<RealVector> curve;
-//        info = ContourMethod::contour2d(this, (Boundary *)boundary,rect, number_of_cells, curve);
+        info = ContourMethod::contour2d(this, curve);
 
         if (curve.size() > 0){
             eigenvalues_curves.push_back(curve);
@@ -306,19 +211,16 @@ int Eigenvalue_Contour::curve(std::vector< std::vector<RealVector> > &eigenvalue
     return info;
 }
 
-int Eigenvalue_Contour::curve(std::vector<RealVector> &curve, double &l){
+int Eigenvalue_Contour::curve(const FluxFunction *f, const AccumulationFunction *a, 
+                              GridValues &g, 
+                              std::vector<RealVector> &curve, double &l){
+
+    g.fill_eigenpairs_on_grid(f, a);
+    gv = &g;
 
     curve.clear();
 
-    int info;
-
-    double rect[4];
-    rect[0] = pmin.component(0);
-    rect[1] = pmax.component(0);
-    rect[2] = pmin.component(1);
-    rect[3] = pmax.component(1);
-
-//    info = ContourMethod::contour2d(this, (Boundary *) boundary,rect, number_of_cells, curve);
+    int info = ContourMethod::contour2d(this, curve);
     l = level;
 
     return info;
