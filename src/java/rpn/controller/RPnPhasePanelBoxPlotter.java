bf6ea772577e25a4c6f69afbc5bd680564303644
Edson Landim de Almeida <edsonlan@impa.br>
2012-09-10 14:32:32 -0300

    Selecao de area

--- src/java/rpn/controller/RPnPhasePanelBoxPlotter.java
+++ src/java/rpn/controller/RPnPhasePanelBoxPlotter.java
@@ -8,8 +8,16 @@ import java.awt.Point;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
+import java.awt.geom.Path2D;
 import java.awt.geom.Rectangle2D;
 import rpn.RPnPhaseSpacePanel;
+import wave.multid.Coords2D;
+import wave.multid.CoordsArray;
+import wave.multid.Space;
+import wave.multid.map.Map;
+import wave.multid.view.Viewing2DTransform;
+import wave.multid.view.ViewingTransform;
+import wave.util.RealMatrix2;
 
 /**
  *
@@ -27,8 +35,19 @@ public class RPnPhasePanelBoxPlotter implements MouseMotionListener, MouseListen
         if (addRectangle_) {
             RPnPhaseSpacePanel panel = (RPnPhaseSpacePanel) me.getSource();
 
+            Path2D.Double areaSelection = new Path2D.Double();
+            
+            ViewingTransform viewingTransform = panel.scene().getViewingTransform();
+            
+
+
+
             double x = cursorPos_.getX();
             double y = cursorPos_.getY();
+
+            //P1
+            areaSelection.moveTo(x, y);
+
 
             double newx = me.getX();
             double newy = me.getY();
@@ -36,8 +55,40 @@ public class RPnPhasePanelBoxPlotter implements MouseMotionListener, MouseListen
             double w = Math.abs(newx - x);
             double h = Math.abs(newy - y);
 
+
             x = Math.min(x, newx);
             y = Math.min(y, newy);
+
+            //P2
+            
+            
+            Map testeMap =             viewingTransform.viewingMap();
+            
+            
+            RealMatrix2 matrixTeste = testeMap.getTransfMatrix();
+            
+            
+            System.out.println(matrixTeste);
+            
+            
+//            
+//            double tempDC [] = {cursorPos_.getX(),cursorPos_.getY()};
+//            Coords2D inDC  = new Coords2D(tempDC);
+//            
+//            CoordsArray outWC  = new CoordsArray(new Space("", 2));
+//            
+//            viewingTransform.dcInverseTransform(inDC,outWC);
+//            
+//            
+//            Coords2D finalDC = new Coords2D();
+//            
+//            viewingTransform.viewPlaneTransform(outWC, finalDC);
+//
+//
+////            System.out.println("in: "+ inDC.getCoords()[0]+" "+ inDC.getCoords()[1]+ "out: "+  outWC.getCoords()[0] +" "+outWC.getCoords()[1]);
+//            
+//            System.out.println("inWC: "+ outWC.getCoords()[0]+" "+ outWC.getCoords()[1]+ "out: "+  outWC.getCoords()[0] +" "+outWC.getCoords()[1]);
+
 
             int listSize = panel.getCastedUI().getSelectionAreas().size();
 
