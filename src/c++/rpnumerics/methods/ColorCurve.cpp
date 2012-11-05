9d221ea218fbf567287e7f95fa9d829751441c6f
Edson Landim de Almeida <edsonlan@impa.br>
2012-11-05 14:50:01 -0200

    Testando classificacao da curva de Hugoniot  na fisica tpcw

--- src/c++/rpnumerics/methods/ColorCurve.cpp
+++ src/c++/rpnumerics/methods/ColorCurve.cpp
@@ -418,6 +418,8 @@ int ColorCurve::complete_point(RealVector &p, double &s, std::vector<double> &ei
     // F, JF, G & JG at p:
     //
     double F[dim], G[dim], JF[dim][dim], JG[dim][dim];
+    
+    cout<<"Ponto p: "<<p<<endl;
 
     fluxFunction_->fill_with_jet(dim, p.components(), 1, F, &JF[0][0], 0);
     accFunction_->fill_with_jet(dim, p.components(), 1, G, &JG[0][0], 0);
@@ -439,6 +441,8 @@ int ColorCurve::complete_point(RealVector &p, double &s, std::vector<double> &ei
         num += (F[i] - F_ref[i]) * delta_G;
 
         den += delta_G*delta_G;
+        
+         cout<<" G:"<<G[i]<<" F:"<<F[i]<<" Fref:"<<F_ref[i]<<" Gref:"<<G_ref[i]<<endl;
     }
 
     if (fabs(den) > 1e-20) s = num / den;
