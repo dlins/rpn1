4fefeb27e373cda774958a891c8c1e10471dfdff
Edson Landim de Almeida <edsonlan@impa.br>
2012-11-06 11:57:19 -0200

    Metodo toXML para o command de mudanca de parametros de fluxo

--- src/java/rpn/controller/ui/UIController.java
+++ src/java/rpn/controller/ui/UIController.java
@@ -403,7 +403,6 @@ public class UIController extends ComponentUI {
     public void logCommand(RpCommand command) {
         RPnUIFrame.clearStatusMessage();
         commandArray_.add(command);
-        System.out.println(command.toXML());
     }
 
     /** Sets the state of the application. The application works as a state machine and this method changes the actual state.*/
