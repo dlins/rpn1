118c114d0d1fed421751f492dd0ab69a40b736fb
Edson Landim de Almeida <edsonlan@impa.br>
2013-12-05 15:20:55 -0200

    Incluindo comandos para extensao generica

--- src/java/rpn/parser/RPnCommandModule.java
+++ src/java/rpn/parser/RPnCommandModule.java
@@ -257,6 +257,9 @@ public class RPnCommandModule {
                 } else if (currentCommand_.equalsIgnoreCase("curveselection")) {
                     selectedGeometry_ = selectCurve(curveId_);
                     System.out.println("Selecionando curva :"+curveId_);
+                    RPnPhaseSpaceFrame frame =(RPnPhaseSpaceFrame)RPnUIFrame.getFrame("Axis0 Axis1");
+                    
+                    curvesPanel_= frame.phaseSpacePanel();
 
                     GenericExtensionCurveCommand.instance().setGeometryAndPanel(selectedGeometry_, curvesPanel_);
 
