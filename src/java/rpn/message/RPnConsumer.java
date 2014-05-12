9d404b46293ba9cb25d2633b990e92ee728a36df
Edson Landim de Almeida <edsonlan@impa.br>
2014-05-12 19:02:41 -0300

    Testando uso dos comandos

--- src/java/rpn/message/RPnConsumer.java
+++ src/java/rpn/message/RPnConsumer.java
@@ -241,6 +241,7 @@ public class RPnConsumer implements RPnResetableListener {
 
                 // COMMAND MESSAGES PARSING
                 RPnCommandModule.init(XMLReaderFactory.createXMLReader(), new StringBufferInputStream(text));
+                System.out.println(text);
 
                 // updates the PhaseSpace Frames...
                 rpn.RPnPhaseSpaceFrame[] framesList = rpn.RPnUIFrame.getPhaseSpaceFrames();
