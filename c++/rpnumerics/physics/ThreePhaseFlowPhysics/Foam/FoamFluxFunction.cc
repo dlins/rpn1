7b1a4691aac1bf4d774e11673b15129053bde07d
Rodrigo Morante <morante@impa.br>
2014-10-29 18:18:19 -0200

    Foam's Hessian was corrected.

--- c++/rpnumerics/physics/ThreePhaseFlowPhysics/Foam/FoamFluxFunction.cc
+++ c++/rpnumerics/physics/ThreePhaseFlowPhysics/Foam/FoamFluxFunction.cc
@@ -64,7 +64,7 @@ int FoamFluxFunction::jet(const RealVector &w, JetMatrix &m, int degree) const {
         //
         double mug = mug_jet.get(0);
         double kg = kg_jet.get(0);
-        double lkg = kg/mug;
+        double lkg = kg/mug; // ***
 
         // Total mobility.
         //
@@ -76,12 +76,12 @@ int FoamFluxFunction::jet(const RealVector &w, JetMatrix &m, int degree) const {
         if (degree >= 1){ 
            double ldkw_dsw = kw_jet.get(0, 0)/muw; // dkw_dsw = kw_jet.get(0, 0)
            double ldko_dsw = ko_jet.get(0, 0)/muo; // dko_dsw = ko_jet.get(0, 0)
-           double ldkg_dsw = kg_jet.get(0, 0)/mug  -  lkg*mug_jet.get(0, 0)/mug; // dkg_dsw = kg_jet.get(0, 0), dmug_dsw = mug_jet.get(0, 0)
+           double ldkg_dsw = kg_jet.get(0, 0)/mug  -  lkg*mug_jet.get(0, 0)/mug; // dkg_dsw = kg_jet.get(0, 0), dmug_dsw = mug_jet.get(0, 0) ***
            double ldk_dsw  = ldkw_dsw + ldko_dsw + ldkg_dsw;
 
            double ldkw_dso = kw_jet.get(0, 1)/muw; // dkw_dsw = kw_jet.get(0, 0)
            double ldko_dso = ko_jet.get(0, 1)/muo; // dko_dsw = ko_jet.get(0, 0)
-           double ldkg_dso = kg_jet.get(0, 1)/mug  -  lkg*mug_jet.get(0, 1)/mug; // dkg_dsw = kg_jet.get(0, 0), dmug_dsw = mug_jet.get(0, 0)
+           double ldkg_dso = kg_jet.get(0, 1)/mug  -  lkg*mug_jet.get(0, 1)/mug; // dkg_dsw = kg_jet.get(0, 0), dmug_dsw = mug_jet.get(0, 0) ***
            double ldk_dso  = ldkw_dso + ldko_dso + ldkg_dso;
 
            double tw = vel + lko*(grw - gro) + lkg*(grw - grg);
@@ -107,24 +107,32 @@ int FoamFluxFunction::jet(const RealVector &w, JetMatrix &m, int degree) const {
                double ld2kw_dswso = kw_jet.get(0, 0, 1)/muw; // d2kw_dswso = kw_jet.get(0, 0, 1)
                double ld2kw_dso2  = kw_jet.get(0, 1, 1)/muw; // d2kw_dso2  = kw_jet.get(0, 1, 1)
 
-               double ld2ko_dsw2  = ko_jet.get(0, 0, 0)/muw; // d2kw_dsw2  = kw_jet.get(0, 0, 0)
-               double ld2ko_dswso = ko_jet.get(0, 0, 1)/muw; // d2kw_dswso = kw_jet.get(0, 0, 1)
-               double ld2ko_dso2  = ko_jet.get(0, 1, 1)/muw; // d2kw_dso2  = kw_jet.get(0, 1, 1)
+               double ld2ko_dsw2  = ko_jet.get(0, 0, 0)/muo; // d2kw_dsw2  = kw_jet.get(0, 0, 0)
+               double ld2ko_dswso = ko_jet.get(0, 0, 1)/muo; // d2kw_dswso = kw_jet.get(0, 0, 1)
+               double ld2ko_dso2  = ko_jet.get(0, 1, 1)/muo; // d2kw_dso2  = kw_jet.get(0, 1, 1)
 
-               double ld2kg_dsw2  = kg_jet.get(0, 0, 0)/mug - kg_jet.get(0, 0)*mug_jet.get(0, 0)/mug; // d2kg_dsw2/mug -    ldkg_dsw*dmugdsw/mug ;
-               double ld2kg_dswso = kg_jet.get(0, 0, 1)/mug - ldkg_dsw*mug_jet.get(0, 1)/mug ;
-               double ld2kg_dso2  = ko_jet.get(0, 1, 1)/mug - ldkg_dso*mug_jet.get(0, 1)/mug ;
+               double ld2kg_dsw2  = kg_jet.get(0, 0, 0)/mug - kg_jet.get(0, 0)*mug_jet.get(0, 0)/(mug*mug) - 
+                                    (dlkg_dsw*mug_jet.get(0, 0) + lkg*mug_jet.get(0, 0, 0))/mug + 
+                                    (lkg*mug_jet.get(0, 0)*mug_jet.get(0, 0))/(mug*mug); // ***
+
+               double ld2kg_dswso = kg_jet.get(0, 0, 1)/mug - kg_jet.get(0, 0)*mug_jet.get(0, 1)/(mug*mug) - 
+                                    (dlkg_dso*mug_jet.get(0, 0) + lkg*mug_jet.get(0, 0, 1))/mug + 
+                                    (lkg*mug_jet.get(0, 0)*mug_jet.get(0, 1))/(mug*mug); // ***
+
+               double ld2kg_dso2  = kg_jet.get(0, 1, 1)/mug - kg_jet.get(0, 1)*mug_jet.get(0, 1)/(mug*mug) - 
+                                    (dlkg_dso*mug_jet.get(0, 1) + lkg*mug_jet.get(0, 1, 1))/mug + 
+                                    (lkg*mug_jet.get(0, 1)*mug_jet.get(0, 1))/(mug*mug); // ***
 
                double ld2k_dsw2  = ld2kw_dsw2 + ld2ko_dsw2 + ld2kg_dsw2; 
                double ld2k_dswso = ld2kw_dswso + ld2ko_dswso + ld2kg_dswso;
                double ld2k_dso2  = ld2kw_dso2 + ld2ko_dso2 + ld2kg_dso2;
 
-               double zgfww =( ( lk * ld2kw_dsw2 - lkw * ld2k_dsw2 ) / lk  -  2. * ldk_dsw * zgwdsw )/ lk;
+               double zgfww =( ( lk * ld2kw_dsw2 - lkw * ld2k_dsw2 )   / lk  -  2. * ldk_dsw * zgwdsw )/ lk;
                double zgfwo =( ( lk * ld2kw_dswso - lkw * ld2k_dswso ) / lk  -  (ldk_dsw * zgwdso + zgwdsw * ldk_dso) ) / lk;
-               double zgfoo =( ( lk * ld2kw_dso2 - lkw * ld2k_dso2 ) / lk  -  2. * ldk_dso * zgwdso )/ lk;
-               double zggww =( ( lk * ld2ko_dsw2 - lko * ld2k_dsw2 ) / lk  -  2. * ldk_dsw * zgodsw )/ lk;
+               double zgfoo =( ( lk * ld2kw_dso2 - lkw * ld2k_dso2 )   / lk  -  2. * ldk_dso * zgwdso )/ lk;
+               double zggww =( ( lk * ld2ko_dsw2 - lko * ld2k_dsw2 )   / lk  -  2. * ldk_dsw * zgodsw )/ lk;
                double zggwo =( ( lk * ld2ko_dswso - lko * ld2k_dswso ) / lk  -  (ldk_dsw * zgodso + zgodsw * ldk_dso) ) / lk;
-               double zggoo =( ( lk * ld2ko_dso2 - lko * ld2k_dso2 ) / lk  -  2. * ldk_dso * zgodso )/ lk;
+               double zggoo =( ( lk * ld2ko_dso2 - lko * ld2k_dso2 )   / lk  -  2. * ldk_dso * zgodso )/ lk;
           
                double dtwdww = ld2ko_dsw2*(grw - gro) + (grw - grg)*ld2kg_dsw2;
                double dtwdwo = ld2ko_dswso*(grw - gro) + (grw - grg)*ld2kg_dswso;
